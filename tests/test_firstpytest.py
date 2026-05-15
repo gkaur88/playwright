aac6fa78e4f3f275686a86e57ecab4d54ae17669
Gurjeet Kaur <kaur.gurjeet88@gmail.com>
2026-05-15 17:04:46 +0530

    change user

--- tests/test_firstpytest.py
+++ tests/test_firstpytest.py
@@ -1,6 +1,6 @@
 import pytest
 @pytest.mark.parametrize("a,b", [(2,4), (2,3), (3,2), (4,1)])
 
-@pytest.mark.skip(reason="already tested")
+@pytest.mark.skip(reason="already check ted")
 def test_add(a,b):
     assert a+b == 5
